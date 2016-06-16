94ce8a94ad29e712bacfe63bdade694b7e1dab32
Daniel Hill <git@DanielAHill.com>
2016-06-15 20:47:01 -0600

    Filter based on requested version

--- src/DanielAHill.AspNet.ApiActions.Swagger/Creation/ISwaggerApiActionRegistrationProvider.cs
+++ src/DanielAHill.AspNet.ApiActions.Swagger/Creation/ISwaggerApiActionRegistrationProvider.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using DanielAHill.AspNet.ApiActions.Versioning;
 
 namespace DanielAHill.AspNet.ApiActions.Swagger.Creation
@@ -7,21 +6,5 @@ namespace DanielAHill.AspNet.ApiActions.Swagger.Creation
     public interface ISwaggerApiActionRegistrationProvider
     {
         IReadOnlyCollection<IApiActionRegistration> Get(ApiActionVersion version);
-    }
-
-    public class SwaggerApiActionRegistrationProvider : ISwaggerApiActionRegistrationProvider
-    {
-        private readonly IApiActionRegistrationProvider _registrationProvider;
-
-        public SwaggerApiActionRegistrationProvider(IApiActionRegistrationProvider registrationProvider)
-        {
-            if (registrationProvider == null) throw new ArgumentNullException(nameof(registrationProvider));
-            _registrationProvider = registrationProvider;
-        }
-
-        public IReadOnlyCollection<IApiActionRegistration> Get(ApiActionVersion version)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
---
+++ src/DanielAHill.AspNet.ApiActions.Swagger/Creation/SwaggerApiActionRegistrationProvider.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DanielAHill.AspNet.ApiActions.Versioning;
+
+namespace DanielAHill.AspNet.ApiActions.Swagger.Creation
+{
+    public class SwaggerApiActionRegistrationProvider : ISwaggerApiActionRegistrationProvider
+    {
+        private readonly IApiActionRegistrationProvider _registrationProvider;
+        private readonly IVersionEdgeProvider _versionEdgeProvider;
+        private IReadOnlyCollection<Tuple<ApiActionVersion, ApiActionVersion, IApiActionRegistration>> _registrationPreCalcs;
+
+        public SwaggerApiActionRegistrationProvider(IApiActionRegistrationProvider registrationProvider, IVersionEdgeProvider versionEdgeProvider)
+        {
+            _registrationProvider = registrationProvider;
+            _versionEdgeProvider = versionEdgeProvider;
+            if (registrationProvider == null) throw new ArgumentNullException(nameof(registrationProvider));
+            if (versionEdgeProvider == null) throw new ArgumentNullException(nameof(versionEdgeProvider));
+        }
+
+        public IReadOnlyCollection<IApiActionRegistration> Get(ApiActionVersion version)
+        {
+            if (_registrationPreCalcs == null)
+            {
+                _registrationPreCalcs = GeneratePreCalcs(_registrationProvider, _versionEdgeProvider);
+            }
+
+            return _registrationPreCalcs.Where(i => (i.Item1 == null || i.Item1 <= version) && (i.Item2 == null || i.Item2 >= version)).Select(i => i.Item3).ToList();
+        }
+
+        protected virtual IReadOnlyCollection<Tuple<ApiActionVersion, ApiActionVersion, IApiActionRegistration>> GeneratePreCalcs(IApiActionRegistrationProvider registrationProvider, IVersionEdgeProvider versionEdgeProvider)
+        {
+            var preCalc = new List<Tuple<ApiActionVersion, ApiActionVersion, IApiActionRegistration>>();
+            var registrations = registrationProvider.Registrations;
+            var walkingArray = new Type[1];
+            
+            foreach (var reg in registrations)
+            {
+                walkingArray[0] = reg.ApiActionType;
+                var edges = versionEdgeProvider.GetVersionEdges(walkingArray);
+
+                if (edges == null || edges.Length == 0)
+                {
+                    preCalc.Add(new Tuple<ApiActionVersion, ApiActionVersion, IApiActionRegistration>(null, null, reg));
+                }
+                else if (edges.Length == 1)
+                {
+                    preCalc.Add(new Tuple<ApiActionVersion, ApiActionVersion, IApiActionRegistration>(edges[0], edges[0], reg));
+                }
+                else if (edges.Length == 2)
+                {
+                    var minEdge = edges[0];
+                    var maxEdge = edges[1];
+
+                    if (minEdge > maxEdge)
+                    {
+                        minEdge = edges[1];
+                        maxEdge = edges[0];
+                    }
+
+                    preCalc.Add(new Tuple<ApiActionVersion, ApiActionVersion, IApiActionRegistration>(minEdge, maxEdge, reg));
+                }
+                else
+                {
+                    // TODO?: Log warning
+                }
+            }
+
+            return preCalc.ToArray();
+        }
+    }
+}
